727f155626eb001f70823b4c374d80de96c9c121
Bruno Donatelli <bruno.donatelli17@gmail.com>
2023-10-11 16:47:09 -0300

    chore: add initial project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rinha-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rinha-frontend2';
+}
